244bf5d465173933c3ccbe26a88f2105a4dda5b2
rusty-reebs <rusty_reebs@tutanota.com>
2021-11-18 16:44:18 +0400

    modify increaseNavbarCart function with quantity parameter

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 import App from "./App";
 import Products from "./components/Products";
+import Navbar from "./components/Navbar";
 
 const RouteSwitch = () => {
+  const [currentCart, setCart] = useState(0);
+
+  const increaseNavbarCart = (quantity) => {
+    setCart(currentCart + quantity); // currentCart + quantity
+  };
+
+  const decreaseNavbarCart = () => {
+    setCart(currentCart - 1);
+  };
+
   return (
     <BrowserRouter>
+      <Navbar currentCart={currentCart} />
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/products" element={<Products />} />
+        <Route
+          path="/products"
+          element={
+            <Products
+              increaseNavbarCart={increaseNavbarCart}
+              decreaseNavbarCart={decreaseNavbarCart}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
